97f8b36131c1746a7f834b8b81783aa8fdec1fff
albertrein <guilhermealberto8@gmail.com>
2018-12-02 01:05:28 -0200

    Subindo primeira versão do jogo.

---
+++ JS/dadosDoJogo.js
@@ -0,0 +1,40 @@
+const dadosDoJogo = {
+	"info":[
+		{
+			"id": 0,
+			"img": "media/img1.png",
+			"audio": "media/gravacoes/audio.mp3",
+			"text": "This descroption of text"
+		},
+		{
+			"id": 1,
+			"img": "media/img2.png",
+			"audio": "media/gravacoes/audio.mp3",
+			"text": "This descroption of text"
+		},
+		{
+			"id": 2,
+			"img": "media/img3.png",
+			"audio": "media/gravacoes/audio.mp3",
+			"text": "This descroption of text"
+		},
+		{
+			"id": 3,
+			"img": "media/img4.png",
+			"audio": "media/gravacoes/audio.mp3",
+			"text": "This descroption of text"
+		},
+		{
+			"id": 4,
+			"img": "media/img5.png",
+			"audio": "media/gravacoes/audio.mp3",
+			"text": "This descroption of text"
+		},
+		{
+			"id": 5,
+			"img": "media/img6.png",
+			"audio": "media/gravacoes/audio.mp3",
+			"text": "This descroption of text1"
+		}
+	]
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# JogoDaMemoriaEducacional
+# JogoDaMemoriaEducacional :grey_exclamation: :thumbsup:
 This is a project to teach something in game formate.
